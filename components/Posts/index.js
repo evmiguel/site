87cfe5b72247176c752dd5ec6d9eace94386fa44
Erika Miguel <erika@erikamiguel.com>
2024-04-03 13:16:42 -0400

    Use css modules for blog page

--- components/Posts/index.js
+++ components/Posts/index.js
@@ -1,14 +1,16 @@
 import Link from "next/link";
 import dayjs from "dayjs";
+import styles from './Posts.module.scss';
+
 
 const Posts = ({ posts: { nodes }}) => {
     return (
-        <ul className="posts">
+        <ul className={styles.posts}>
             {
                 nodes.map((node) => {
                     return (
-                        <li className="posts__entry">
-                            <h1 className="posts__entry-title-link"><Link href={`/blog/${node.slug}`}><a className="posts__entry-title-link">{node.title}</a></Link></h1>
+                        <li className={styles.posts__entry}>
+                            <h1 className={styles.posts__entry_title}><Link href={`/blog/${node.slug}`}><a className={styles.posts__entry_title_link}>{node.title}</a></Link></h1>
                             <h2>{node.author.node.firstName} {node.author.node.lastName}</h2>
                             <h3>{dayjs(node.date).format('MMMM D, YYYY')}</h3>
                         </li>
