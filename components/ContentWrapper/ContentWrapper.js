540d1e2ff304e777a532f551a52ceb0134464388
Erika Miguel <erika@erikamiguel.com>
2024-05-16 00:08:31 -0400

    Styles for heading

--- components/ContentWrapper/ContentWrapper.js
+++ components/ContentWrapper/ContentWrapper.js
@@ -5,7 +5,7 @@ let cx = className.bind(styles);
 
 export default function ContentWrapper({ content, children, className }) {
   return (
-    <article className={cx(['component', className])}>
+    <article className={`${cx(['component', className])} [&_h2]:text-4xl`}>
       <div dangerouslySetInnerHTML={{ __html: content ?? '' }} />
       {children}
     </article>
