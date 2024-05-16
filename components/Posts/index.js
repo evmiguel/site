ad147c34b16b3c8e7429cad210ca510df9915e8e
Erika Miguel <erika@erikamiguel.com>
2024-05-15 22:40:28 -0400

    Use tailwind for styles

--- components/Posts/index.js
+++ components/Posts/index.js
@@ -1,18 +1,17 @@
 import Link from "next/link";
 import dayjs from "dayjs";
-import styles from './Posts.module.scss';
 
 
 const Posts = ({ posts: { nodes }}) => {
     return (
-        <ul className={styles.posts}>
+        <ul className='p-[3rem]'>
             {
                 nodes.map((node) => {
                     return (
-                        <li className={styles.posts__entry}>
-                            <h1 className={styles.posts__entry_title}><Link href={`/blog/${node.slug}`}><a className={styles.posts__entry_title_link}>{node.title}</a></Link></h1>
-                            <h2>{node.author.node.firstName} {node.author.node.lastName}</h2>
-                            <h3>{dayjs(node.date).format('MMMM D, YYYY')}</h3>
+                        <li className='text-[3rem] [&:not(:last-child)]:mb-[3rem] [&:not(:last-child)]:pb-[4rem] [&:not(:last-child)]:border-b-[1px] [&:not(:last-child)]:border-black'>
+                            <h1 className='visited:text-slate-900 text-slate-800'><Link href={`/blog/${node.slug}`}><a className=''>{node.title}</a></Link></h1>
+                            <h2 className='text-[2rem]'>{node.author.node.firstName} {node.author.node.lastName}</h2>
+                            <h3 className='text-[1.5rem]'>{dayjs(node.date).format('MMMM D, YYYY')}</h3>
                         </li>
                     )
                 })
